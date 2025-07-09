b7548eb249f6d711b3862ac43c8f39726e223a55
1F10230058 <s1f102300582@iniad.org>
2025-07-10 02:13:57 +0900

    当たり判定の実装

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,11 +7,12 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(0.1f, 0, 0);
@@ -19,6 +20,19 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-0.1f, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // ぶつかった相手のオブジェクトに "Obstacle" というタグが付いているかチェック
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            // もし付いていれば、コンソールにメッセージを表示
+            Debug.Log("障害物に当たった！ゲームオーバー！");
+
+            // ここで自分のオブジェクトを破壊して、ゲームオーバーを表現
+            Destroy(gameObject);
         }
     }
 }
