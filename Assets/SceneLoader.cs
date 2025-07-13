ec715af172ba680f29f32dc6f003eadf78104643
1F10230058 <s1f102300582@iniad.org>
2025-07-14 02:44:33 +0900

    終了ボタンを実装

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -1,4 +1,3 @@
-// SceneLoader.cs
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,5 +6,17 @@ public class SceneLoader : MonoBehaviour
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    // ↓↓↓ このメソッドを追記 ↓↓↓
+    public void QuitGame()
+    {
+        // Unityエディタで実行している場合
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        // ビルドしたゲームで実行している場合
+#else
+        Application.Quit();
+#endif
     }
 }
