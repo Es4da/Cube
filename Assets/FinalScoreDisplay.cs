d8008abe4a83320ac673725e758bfa098c3649a4
1F10230058 <s1f102300582@iniad.org>
2025-07-18 22:59:01 +0900

    ファイル修正

--- Assets/FinalScoreDisplay.cs
+++ Assets/FinalScoreDisplay.cs
@@ -1,16 +1,20 @@
+// FinalScoreDisplay.cs
 using UnityEngine;
 using TMPro;
 
 public class FinalScoreDisplay : MonoBehaviour
 {
     public TextMeshProUGUI finalScoreText;
+    public TextMeshProUGUI highScoreText; // ハイスコア表示用のUIテキストを追加
 
     void Start()
     {
-        // "LastScore"というキー名で保存されたスコアを読み込む
+        // 今回のスコアを表示 (既存の処理)
         float lastScore = PlayerPrefs.GetFloat("LastScore", 0f);
-
-        // UIテキストに表示する（小数点以下1桁まで）
         finalScoreText.text = "あなたの記録: " + lastScore.ToString("F1") + " 秒";
+
+        // ハイスコアを表示 (ここから追加)
+        float highScore = PlayerPrefs.GetFloat("HighScore", 0f);
+        highScoreText.text = "ハイスコア: " + highScore.ToString("F1") + " 秒";
     }
 }
